a7e6da86d7b79c838d35cb4d9c4b7591e91a7f7d
Rudra Pratap Singh <77040722+R-prat@users.noreply.github.com>
2022-08-31 21:53:20 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,6 @@
+import c,{a,b,d}from './module2.mjs'
+console.log(c);
+console.log(a);
+console.log(b);
+console.log(d);
+
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Harry";
+const b = "Rudra";
+const c = "Jugni";
+const d = "priyanka";
+
+export default c;
+export {a};
+export {b};
+export {d};
